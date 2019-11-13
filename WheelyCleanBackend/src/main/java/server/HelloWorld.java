fdb2ad0e2aef190e8ae9afd3872b8bc61a2a09e7
Felix <felix.lapierre1@gmail.com>
2019-11-13 11:16:03 -0500

    Initial setup for Gradle with basic tested class

---
+++ WheelyCleanBackend/src/main/java/server/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+/**
+ *
+ * @author Felix
+ */
+public class HelloWorld {
+    public static String GetMessage() {
+        return "Hello, world!";
+    }
+}
---
+++ WheelyCleanBackend/src/test/java/server/HelloWorldTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Felix
+ */
+public class HelloWorldTest {
+    
+    public HelloWorldTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of GetMessage method, of class HelloWorld.
+     */
+    @Test
+    public void testGetMessage() {
+        System.out.println("GetMessage");
+        String expResult = "Hello, world!";
+        String result = HelloWorld.GetMessage();
+        assertEquals(expResult, result);
+    }
+    
+}
