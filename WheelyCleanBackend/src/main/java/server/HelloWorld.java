b329ef3a8346b0dc0028582a8287f5351769b905
Felix <felix.lapierre1@gmail.com>
2019-11-13 11:23:31 -0500

    Removed useless header comments

--- WheelyCleanBackend/src/main/java/server/HelloWorld.java
+++ WheelyCleanBackend/src/main/java/server/HelloWorld.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package server;
 
 /**
- *
+ * Basic hello world class to validate that Gradle works.
  * @author Felix
  */
 public class HelloWorld {
--- WheelyCleanBackend/src/test/java/server/HelloWorldTest.java
+++ WheelyCleanBackend/src/test/java/server/HelloWorldTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package server;
 
 import org.junit.After;
@@ -13,7 +8,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
+ * Test class for HelloWorld
  * @author Felix
  */
 public class HelloWorldTest {
